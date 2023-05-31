334e78da7b4e0921690152343340e4d770e3866e
Pramod5858 <108333259+Pramod5858@users.noreply.github.com>
2023-05-31 11:11:09 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose")
 const cors = require("cors");
+const BASE_URL = process.env.BASE_URL;
 
 const route = require('./Route/index');
 
-const port = 5500;
-const hostname = "localhost";
+const port = process.env.PORT || 5500;
+//const hostname = "localhost";
 //const dbUrl = "mongodb://127.0.0.1:27017/chaiwala";
-const atlasUrl = "mongodb+srv://chaiwalauser:PXIHaR0QmikfU2e8@cluster0.dznj06x.mongodb.net/chaiwalaShop?retryWrites=true&w=majority";
+const DATABASE = "mongodb+srv://chaiwalauser:PXIHaR0QmikfU2e8@cluster0.dznj06x.mongodb.net/chaiwalaShop?retryWrites=true&w=majority";
 
 // project Name = chaiwala
 // link= 'mongodb+srv://chaiwalauser:<password>@cluster0.dznj06x.mongodb.net/?retryWrites=true&w=majority'
@@ -29,11 +30,11 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 app.use('/',route);
-mongoose.connect(atlasUrl, {useNewUrlParser:true, useUnifiedTopology:true})
+mongoose.connect(DATABASE, {useNewUrlParser:true, useUnifiedTopology:true})
 .then(
     res=>{
-        app.listen(port, hostname, ()=>{
-            console.log(`Server is connected ${hostname}:${port}`);
+        app.listen(port, BASE_URL, ()=>{
+            console.log(`Server is connected ${BASE_URL}:${port}`);
         })
     }
 )
